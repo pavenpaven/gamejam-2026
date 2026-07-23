81ad69d9c65f97263fcba222f8272a9fb1bd206b
Mattias Burman <mattias@asus-tuf>
2026-07-23 16:18:49 +0200

    now

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
-@onready var camera = $Camera2D
-@onready var board  = $Board
+@onready var camera      = $Camera2D
+@onready var board       = $Board
+@onready var ingredients = [$Ingredient]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,10 +10,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	for ingredient in ingredients:
+		if ingredient.grabbed:
+			ingredient.position = get_real_pos(get_viewport().get_mouse_position())
+
+func drop(ingredient):
 	pass
+	
+func get_real_pos(pos):
+	return (pos - get_viewport().get_visible_rect().size/2) / camera.zoom
 
 func get_board_pos(pos):
-	return ((pos -  get_viewport().get_visible_rect().size/2) / camera.zoom - board.position) / board.tilesz
+	return (get_real_pos(pos) - board.position) / board.tilesz
 
 func _input(event):
 	if event is InputEventMouseButton:
--- Scripts/ingredient.gd
+++ Scripts/ingredient.gd
@@ -3,10 +3,36 @@ extends Node2D
 @export  var type   = 0
 @onready var sprite = $Sprite2D
 
+var mouse_on        = false
+var grabbed         = false
+var onboard         = false
+var origin          
+
 func _ready() -> void:
+	origin = position
 	if type < len(Globals.ingredient_texs):
-		sprite.texture = Globals.ingredient_texs[type]
+		sprite.texture = Globals.ingredient_back_texs[type]
 
 
 func _process(delta: float) -> void:
-	pass
+	if grabbed:
+		sprite.texture = Globals.ingredient_texs[type]
+	else:
+		sprite.texture = Globals.ingredient_back_texs[type]
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == 1:
+			if event.pressed:
+				if mouse_on:
+					grabbed = true
+			else:
+				grabbed = false
+				get_parent().drop(self)
+				
+
+func _on_mouse_entered() -> void:
+	mouse_on = true
+
+func _on_mouse_exited() -> void:
+	mouse_on = false
