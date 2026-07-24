60fd41f7ba4e29b27679dec48bebc182ca5973fb
Mattias Burman <mattias@asus-tuf>
2026-07-24 20:22:05 +0200

    char

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
+@onready var sprite = $AnimatedSprite2D
+
+var character_id 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	sprite.animation = "normal_" + str(character_id)
+	sprite.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
